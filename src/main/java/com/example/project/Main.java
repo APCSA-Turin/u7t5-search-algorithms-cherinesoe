d652eafc54fa9650950c99fcf747da5b7594850d
Cherine Soewignjo <148895697+cherinesoe@users.noreply.github.com>
2025-02-18 05:45:03 +0000

    lets go

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,18 +1,15 @@
 package com.example.project;
 import java.util.ArrayList;
 
+import com.example.project.Binary_Search.BinarySearch;
 import com.example.project.Repair.RepairSchedule;
+import com.example.project.Spell_Checker.SpellChecker;
 
 public class Main {
     public static void main(String[] args) {
-        RepairSchedule schedule = new RepairSchedule(3); 
-        ArrayList<Integer> availableMechanics = schedule.availableMechanics();
-        schedule.addRepair(0, 1);
-        availableMechanics = schedule.availableMechanics();
-        System.out.println(availableMechanics.contains(0));
-        System.out.println(availableMechanics.contains(1));
-        schedule.addRepair(1, 2);
-        availableMechanics = schedule.availableMechanics();
-        System.out.println(availableMechanics.contains(1));
+        SpellChecker checker = new SpellChecker();
+        boolean result = checker.binarySpellCheck("lightered");
+        System.out.println(checker.getLoopCounter());
+        System.out.println(result);
     }
 }
